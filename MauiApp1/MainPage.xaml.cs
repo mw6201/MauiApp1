7e2afd4ce9cc89f9715f653df44087d5a480a90f
michaelwalter <mw@transoffice.onmicrosoft.com>
2025-08-03 17:22:43 +0200

    Initial commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object? sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Artur ist {count} cool";
+		else
+			CounterBtn.Text = $"Artur ist {count} cool";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
